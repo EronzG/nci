ddb060101f668b89f6a5ed8b645b77e0e4c3d235
eoinco <eoin.connolly@gmail.com>
2020-11-06 14:57:03 +0000

    added getTransactionCount

--- lab4_transaction.async.js
+++ lab4_transaction.async.js
@@ -1,9 +1,11 @@
 var Tx = require('ethereumjs-tx').Transaction
-const Web3 = require('web3')
-const web3 = new Web3('https://ropsten.infura.io/v3/[your infura key here]')
 
-const account1 = '0x...' // Your account address 1
-const account2 = '0x...' // Your account address 2
+const Web3 = require('web3')
+const web3 = new Web3('https://ropsten.infura.io/v3/[your infura key]')
+
+
+const account1 = '0x9b14eee99808bab2a4c6492d37b4d771f75b7631' // Your account address 1
+const account2 = '0xe8a43eFC2CE385AbA7465101262b03B0d2489c43' // Your account address 2
 
 const privateKey1 = Buffer.from('...', 'hex')
 const privateKey2 = Buffer.from('...', 'hex')
@@ -11,6 +13,10 @@ const privateKey2 = Buffer.from('...', 'hex')
 
 const sendTransaction = async(raw) => {
   return await web3.eth.sendSignedTransaction(raw)
+}
+
+const getTransactionCount = async(account) => {
+  return await web3.eth.getTransactionCount(account)
 }
 
 const transferFunds = async(account1, account2, amount) => {
@@ -21,14 +27,16 @@ const transferFunds = async(account1, account2, amount) => {
 
   const txObject = {
     nonce:    web3.utils.toHex(txCount),
-    gasLimit: web3.utils.toHex(21000), // reduced gas limit because of ropsten issues
-    gasPrice: web3.utils.toHex(web3.utils.toWei('75', 'gwei')),
+    gasLimit: web3.utils.toHex(21000), 
+    gasPrice: web3.utils.toHex(web3.utils.toWei('30', 'gwei')),
     to: account2,
     value:    web3.utils.toHex(web3.utils.toWei(amount, 'ether')),
   }
 
   const tx = new Tx(txObject, {chain:'ropsten', hardfork: 'petersburg'})
+
   tx.sign(privateKey1)
+
   const serializedTx = tx.serialize()
   const raw = '0x' + serializedTx.toString('hex')
 
@@ -39,7 +47,7 @@ const transferFunds = async(account1, account2, amount) => {
 }
 
 const transfer = async() => {
-  await transferFunds(account1, account2, 0.11)
+  await transferFunds(account1, account2, 0.05)
 }
 
 transfer()
