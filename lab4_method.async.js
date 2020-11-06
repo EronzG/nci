6babc30585433710ace1c80bd50d4cb4c4447493
eoinco <eoin.connolly@gmail.com>
2020-11-06 15:35:57 +0000

    returns tx hash correctly and has enough gas to operate

--- lab4_method.async.js
+++ lab4_method.async.js
@@ -1,16 +1,17 @@
 const Web3 = require('web3')
 var Tx = require('ethereumjs-tx').Transaction
 
-const web3 = new Web3('https://ropsten.infura.io/v3/[your infura key here]')
+const web3 = new Web3('https://ropsten.infura.io/v3/[your infura account]')
 
-const account1 = '0x...' // Your account address 1
-const account2 = '0x..' // Your account address 2
+const account1 = '0x9b14eee99808bab2a4c6492d37b4d771f75b7631' // Your account address 1
+const account2 = '0xe8a43eFC2CE385AbA7465101262b03B0d2489c43' // Your account address 2
 
 const privateKey1 = Buffer.from('...', 'hex')
 const privateKey2 = Buffer.from('...', 'hex')
 
+
 // Read the deployed contract - get the addresss from Etherscan
-const contractAddress = '0x485fa61ca54f128c4b31cc0511d1b5128f668045'
+const contractAddress = '0xF74Cc843D643709869c1dA4E068B3fB57FD71630'
 const contractABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"standard","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}]
 
 const contract = new web3.eth.Contract(contractABI, contractAddress)
@@ -31,8 +32,8 @@ const transferFunds = async(account1, account2, amount) => {
 
   const txObject = {
     nonce:    web3.utils.toHex(txCount),
-    gasLimit: web3.utils.toHex(30000), // Raise the gas limit to a much higher amount
-    gasPrice: web3.utils.toHex(web3.utils.toWei('50', 'gwei')),
+    gasLimit: web3.utils.toHex(50000), // uses about 36,000 gas so add some buffer
+    gasPrice: web3.utils.toHex(web3.utils.toWei('30', 'gwei')),
     to: contractAddress,
     data: contract.methods.transfer(account2, amount).encodeABI()
   }
@@ -43,21 +44,20 @@ const transferFunds = async(account1, account2, amount) => {
   const raw = '0x' + serializedTx.toString('hex')
 
   console.log("about to send transaction")
-  let txHash = await sendTransaction(raw)
-  console.log("err: " + txHash.err)
-  console.log("txHash: " + txHash.txHash)
+  let minedTransaction = await sendTransaction(raw)
+  console.log("transaction hash returned: " + minedTransaction.transactionHash)
 }
 
 // async methods
 const getBalanceOf = async(account) => {
   let balanceOf = await contract.methods.balanceOf(account).call()
-  return "balanceOf: " + balanceOf
+  return `balance of account ${account} is ${balanceOf}`
 }
 
 const go = async() => {
   console.log(await getBalanceOf(account1))
   console.log(await getBalanceOf(account2))
-  await transferFunds(account1, account2, 10)
+  await transferFunds(account1, account2, 100)
 }
 
 go()
