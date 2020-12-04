9a231bcc9c97e78e6d26dbd20d5137b23815e0e3
eoinco <eoin.connolly@gmail.com>
2020-12-04 13:15:16 +0000

    added some info to ecdh.js

---
+++ ecdh.js
@@ -0,0 +1,26 @@
+// Elliptic Curve Diffie Hellman
+
+// Alice and Bob
+
+// Want to share communication securely
+
+// to communicate, they must share a secret
+
+// then use that secret to encrypt traffic
+
+// secret is used an an encryption key (e.g. 32-byte string)
+
+// key exchange
+
+// challenge is doing a secure key exchange
+
+// to exchange a secret, Alice and Bob must have public/private keypairs
+
+// Alice knows Bob's public key
+
+// Bob knows Alice's public key
+
+// Signal Protocol - every communication uses a different encryption key, 
+// that cannot be derived from the previous encryption key
+
+
